0e914c06b695bd62fdf6b693cd598472708a261e
dieu.phamquang <phamquangdieu97@gmail.com>
2026-04-21 23:37:51 +0700

    rolling test

--- app/learning/rolling/page.tsx
+++ app/learning/rolling/page.tsx
@@ -1,7 +1,41 @@
+'use client';
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import {gsap} from 'gsap';
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(SplitText);
+gsap.config({ trialWarn: false } as any);
+
 const RollingPage = () => {
+    const ref = useRef<HTMLDivElement>(null);
+    useGSAP(() => {
+        const repeatCount = 8;
+        const tl = gsap.timeline({ paused: true });
+        const split = new SplitText('.rolling-text', { type: "chars" });
+        split.chars.forEach((obj, i) => {
+            console.log(obj);
+            
+            let txt = (obj as HTMLElement).innerText;
+            let clone = `<div class="cloneText"> ${txt} </div>`;
+            let newHTML = `<div class="originalText"> ${txt} </div>${clone}`;
+            obj.innerHTML = newHTML;
+            gsap.set(obj.childNodes[1], {
+                yPercent: i % 2 === 0 ? -100 : 100
+            });
+            let tween = gsap.to(obj.childNodes, {
+                repeat: repeatCount,
+                ease: "none",
+                yPercent: i % 2 === 0 ? "+=100" : "-=100"
+            });
+            tl.add(tween, 0);
+        });
+        gsap.to(tl, { progress: 1, duration: 4, ease: "power4.inOut" });
+
+    });
   return (
-    <div>
-      <h1>Rolling</h1>
+    <div ref={ref} className="h-[100vh] w-[100vw] flex justify-center items-center">
+      <div className="rolling-text text-[10vw] uppercase font-semibold overflow-hidden leading-none">Rolling</div>
     </div>
   );
 };
